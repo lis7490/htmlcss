046ab698c12b9c62cbdfe9bcac2d192045e3f4b8
Stupnikov Dmitry <d_lis82@mail.ru>
2025-08-25 15:53:39 +0300

    шаблоны и формы flask

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+notes = []
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/дневник_программиста", methods=['POST', 'GET'])
+def diary():
+    form_data = {
+        "Содержание": request.form.get('data')
+    }
+    return render_template("notes.html", form_data=form_data)
 if __name__ == "__main__":
     app.run(debug = True)
